c61464bbad17aed3d60a506bd16923b3c1cb732a
Panos Paganis <mallinanga@gmail.com>
2017-02-26 09:46:18 +0200

    WIP

---
+++ nanga-contact.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Author URI: https://www.vgwebthings.com/
+Author: VG web things
+Description: Simple Contact Form
+Domain Path: /languages/
+Plugin Name: VG web things Contact Form
+Text Domain: nanga-contact
+Version: 1.0.0
+*/
+if ( ! defined('WPINC')) {
+    die;
+}
+require_once 'vendor/autoload.php';
